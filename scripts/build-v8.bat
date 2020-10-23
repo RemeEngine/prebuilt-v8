c19b6cb9676149473cc130f0e2b566f29642e7b3
remtori <lqvu99@gmail.com>
2020-10-24 02:05:36 +0700

    Testing done, intergrate everything together

--- scripts/build-v8.bat
+++ scripts/build-v8.bat
@@ -53,7 +53,7 @@ cd ..
 del /f /s /q package 1>nul
 rmdir /s /q package
 md package
-copy build\obj\libv8_monolith.* package\libv8.*
+copy build\obj\libv8_monolith.lib package\libv8.lib
 xcopy v8\include package\include /E /I
 tar czf build/v8-v%version%-%platform%.tar.gz -C package .
 

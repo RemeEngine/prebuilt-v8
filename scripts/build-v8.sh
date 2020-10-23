a306fde8ac18794985e544d1f12c5bc7525a0172
remtori <lqvu99@gmail.com>
2020-10-23 20:08:03 +0700

    Extract out config file

--- scripts/build-v8.sh
+++ scripts/build-v8.sh
@@ -6,56 +6,11 @@ PATH="$PWD/depot_tools:$PATH"
 platform=$1
 version=${2#"v"}
 
-spec=`cat <<EOF
-solutions = [{
-  'name': 'v8',
-  'url': 'https://chromium.googlesource.com/v8/v8.git',
-  'deps_file': 'DEPS',
-  'managed': False,
-  'custom_deps': {
-    'v8/test/benchmarks/data': None,
-    'v8/test/mozilla/data': None,
-    'v8/test/test262/data': None,
-    'v8/test/test262/harness': None,
-    'v8/test/wasm-js': None,
-    'v8/testing/gmock': None,
-    'v8/third_party/android_tools': None,
-    'v8/third_party/catapult': None,
-    'v8/third_party/colorama/src': None,
-    'v8/third_party/instrumented_libraries': None,
-    'v8/tools/gyp': None,
-    'v8/tools/luci-go': None,
-    'v8/tools/swarming_client': None
-  },
-  'custom_vars': {
-    'build_for_node': True
-  }
-}]
-EOF
-`
+spec=`cat spec.cfg`
 spec="${spec//$'\n'/}"
 
-build_args=`cat <<EOF
-  clang_use_chrome_plugins=false
-  is_component_build=false
-  is_debug=false
-  linux_use_bundled_binutils=false
-  strip_debug_info=true
-  symbol_level=0
-  treat_warnings_as_errors=false
-  use_custom_libcxx=false
-  use_sysroot=false
-  v8_enable_gdbjit=false
-  v8_enable_i18n_support=false
-  v8_enable_test_features=false
-  v8_extra_library_files=[]
-  v8_monolithic=true
-  v8_untrusted_code_mitigations=false
-  v8_use_external_startup_data=false
-  v8_use_snapshot=true
-EOF
-`
-build_args="${build_args//$'\n'/}"
+build_args=`cat ./build_args.cfg`
+build_args="${build_args//$'\n'/ }"
 
 # Resolve platform name
 case $platform in
@@ -90,13 +45,13 @@ cd ..
 
 # Build V8
 cd v8
-# ninja -v -C ../build v8_monolith
+ninja -v -C ../build v8_monolith
 cd ..
 
 # Create package
 rm -rf package
 mkdir package
-# cp build/obj/libv8_monolith.a package/libv8.a
+cp build/obj/libv8_monolith.a package/libv8.a
 cp -r v8/include package/include
 tar czf build/v8-v$version-$platform.tar.gz -C package .
 
